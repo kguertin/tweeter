e9987155276474c4487ae88138e4268f83f1476a
Kevin Guertin <kevin.m.guertin@gmail.com>
2020-02-25 21:00:27 +0000

    add counter functionality and color change

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,1 +1,12 @@
-console.log('a okay');
+$(document).ready(function() {
+  $('textarea').keyup(function() {
+    const $tweetBody = $(this);
+    let $counter = $tweetBody.siblings('.counter');
+    $counter.text(`${140 - $tweetBody.val().length}`);
+    if (Number($counter.text()) < 0) {
+      $counter.addClass('max-chars');
+    } else {
+      $counter.removeClass('max-chars');
+    }
+  })
+});
