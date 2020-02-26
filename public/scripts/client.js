912b8c5e1af92ba655c11ec16b8005771acafe73
Kevin Guertin <kevin.m.guertin@gmail.com>
2020-02-26 19:30:51 +0000

    add ajax get request to render tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,31 +3,6 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const userData =[
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
  const createTweetElements = obj => {
    const date1 = new Date(obj.created_at);
    const date2 = Date.now();
@@ -65,19 +40,24 @@ const userData =[
  };
 
  const submitTweet = () => {
-   $('form').on('submit', function(e){
-
+   $('form').on('submit', e => { 
     e.preventDefault();
     const data = $('textarea').serialize();
-    $.ajax('/tweets', 'method: POST', data)
+    $.ajax({url: '/tweets', method: 'POST', data})
       .then(() => {
         console.log('success')});
    })
- }
+  }
+     const loadTweets = () => {
+      $.ajax({url: '/tweets/', method: 'GET', dataType: 'json'})
+        .then((result) => {
+          renderTweets(result)
+        })
+     }
 
  
  $(document).ready(function() {
-   renderTweets(userData);
    submitTweet();
+   loadTweets();
  })
 
