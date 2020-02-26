6018f1c4caaa735f5109016c5273bac5c2b3e610
Kevin Guertin <kevin.m.guertin@gmail.com>
2020-02-26 23:39:34 +0000

    add nav bounce, nav style and text escape

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,63 +1,87 @@
 /*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
- const createTweetElements = obj => {
-   const date1 = new Date(obj.created_at);
-   const date2 = Date.now();
-   const daysAgo = Math.round((date2 - date1) / 1000 / 60 / 60 / 24);
-   const markup = `
-      <article class="tweet">
-        <header>
-          <div>
-          <img src='${obj.user.avatars}'>
-          <p class="display-name">${obj.user.name}</p>
-          </div>
-          <p class="user-name">${obj.user.handle}</p>
-        </header>
-        <div class="tweet-body">
-          <p>${obj.content.text}</p>
-        </div>
-        <footer>
-          <h4 class="date">${daysAgo} days ago</h4>
-          <span>
-            <i class="fas fa-flag"></i>
-            <i class="fas fa-retweet"></i>
-            <i class="fas fa-heart"></i>
-          </span>
-        </footer>
-      </article>`
+* Client-side JS logic goes here
+* jQuery is already loaded
+* Reminder: Use (and do all your DOM work in) jQuery's document ready function
+*/
+// Markup Functions
+const escape =  function(str) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+}
 
-      return markup;
+const createTweetElements = obj => {
+  const date1 = new Date(obj.created_at);
+  const date2 = Date.now();
+  const daysAgo = Math.round((date2 - date1) / 1000 / 60 / 60 / 24);
+  const markup = `
+    <article class="tweet">
+      <header>
+        <div>
+        <img src='${escape(obj.user.avatars)}'>
+        <p class="display-name">${escape(obj.user.name)}</p>
+        </div>
+        <p class="user-name">${escape(obj.user.handle)}</p>
+      </header>
+      <div class="tweet-body">
+        <p>${escape(obj.content.text)}</p>
+      </div>
+      <footer>
+        <h4 class="date">${escape(daysAgo)} days ago</h4>
+        <span>
+          <i class="fas fa-flag"></i>
+          <i class="fas fa-retweet"></i>
+          <i class="fas fa-heart"></i>
+        </span>
+      </footer>
+    </article>`
+
+  return markup;
  };
 
  const renderTweets = arr => {
    for (let i of arr) {
      const $tweet = createTweetElements(i);
-     $('#published-tweets').append($tweet);
+     $('#published-tweets').prepend($tweet);
    }
  };
 
  const submitTweet = () => {
+
    $('form').on('submit', e => { 
     e.preventDefault();
-    const data = $('textarea').serialize();
-    $.ajax({url: '/tweets', method: 'POST', data})
-      .then(() => {
-        console.log('success')});
-   })
-  }
-     const loadTweets = () => {
-      $.ajax({url: '/tweets/', method: 'GET', dataType: 'json'})
-        .then((result) => {
-          renderTweets(result)
+    
+    if(!$('textarea').val()){
+      alert('Please Enter a Tweet');
+    } else if ($('textarea').val().length > 140){
+      alert('Tweet is Too Long, 140 Characters Only');
+    } else {
+      const data = $('textarea').serialize();
+      $.ajax({url: '/tweets', method: 'POST', data})
+        .then(() => {
+          loadTweets();
+          $('textarea').val('');
+          $('.counter').text(140);
         })
-     }
+      }
+    })
+  }
+  
+  const loadTweets = () => {
+    $.ajax({url: '/tweets/', method: 'GET', dataType: 'json'})
+    .then((result) => {
+      $('#published-tweets').empty();
+        renderTweets(result);
+    })
+  }
 
- 
+
+
+
  $(document).ready(function() {
-   submitTweet();
    loadTweets();
+   submitTweet(); 
  })
+
+
 
